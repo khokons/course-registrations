026ab00a7bd299f02294a82a85359218f86bedb0
khokons <khokon.web9226@gmail.com>
2023-09-16 12:35:35 +0600

    cart static part done

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -5,6 +5,15 @@ const Cart = () => {
     return (
         <div>
             
+            <h1 className='text-lg font-bold text-[#2F80ED]'>Credit Hour Remaining  hr</h1>
+            <hr />
+            <h2 className='text-4xl mb-5 mt-5'>Courses Name</h2>
+            <hr />
+
+
+
+            < hr />
+            <h2 className='mt-5 text-xl text-[#1C1B1BCC]'>Total Credit Hour : hr</h2>
         </div>
     );
 };
